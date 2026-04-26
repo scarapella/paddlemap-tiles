2be0910e9f90bb8ec51159355d4d3eab5cbe842a
scarapella <sean@carapella.net>
2026-04-26 18:35:41 -0400

    align command syntax

--- generate_tiles_delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-cd "$(dirname "$0")"
-
-./generate_tiles.sh $@
-
-#cleanup
-export ZONE=$(curl -s -H "Metadata-Flavor: Google" http://google.internal | xargs basename)
-gcloud compute instances delete $HOSTNAME --zone=$ZONE --quiet
